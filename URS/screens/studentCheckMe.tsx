5224d52d4ab5cdc51c130b1a8cddd92364ea63dc
mvuko01 <mvuko01@fesb.hr>
2024-01-30 23:18:33 +0100

    Changed when class id is received from ESP when creating lecture

--- URS/screens/studentCheckMe.tsx
+++ URS/screens/studentCheckMe.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {Button} from "react-native-paper";
 import {AuthContext} from "../context/AuthContext";
 import axios from "axios";
@@ -13,7 +13,7 @@ import {
   ADVERTISING_NAMES,
   API_URL,
 } from "../bleConfig.js";
-import {formatDateTime} from "../helpers.ts";
+import {formatDateTime, sleep} from "../helpers.ts";
 
 import {
   View,
@@ -65,14 +65,281 @@ export default function StudentCheckMe({navigation}: {navigation: any}) {
   );
   const [ESPSessionData, setESPSessionData] = useState<ESPSessionData>(); //default undefined
 
+  /*BLE setup iz in use effect */
+  useEffect(() => {
+    try {
+      BleManager.start({showAlert: true})
+        .then(() => console.debug("BleManager started."))
+        .catch((error: any) =>
+          console.error("BeManager could not be started.", error),
+        );
+    } catch (error) {
+      console.error("unexpected error starting BleManager.", error);
+      return;
+    }
+    const listeners = [
+      bleManagerEmitter.addListener(
+        "BleManagerDiscoverPeripheral",
+        handleDiscoverPeripheral,
+      ),
+      bleManagerEmitter.addListener("BleManagerStopScan", handleStopScan),
+      bleManagerEmitter.addListener(
+        "BleManagerDisconnectPeripheral",
+        handleDisconnectedPeripheral,
+      ),
+      bleManagerEmitter.addListener(
+        "BleManagerDidUpdateValueForCharacteristic",
+        handleUpdateValueForCharacteristic,
+      ),
+      bleManagerEmitter.addListener(
+        "BleManagerConnectPeripheral",
+        handleConnectPeripheral,
+      ),
+    ];
+    handleAndroidPermissions();
+    return () => {
+      console.debug("[app] main component unmounting. Removing listeners...");
+      for (const listener of listeners) {
+        listener.remove();
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleStopScan = () => {
+    setIsScanning(false);
+    console.debug("[handleStopScan] scan is stopped.");
+  };
+  const handleDisconnectedPeripheral = (
+    event: BleDisconnectPeripheralEvent,
+  ) => {
+    console.debug(
+      `[handleDisconnectedPeripheral][${event.peripheral}] disconnected.`,
+    );
+    setPeripherals(map => {
+      let p = map.get(event.peripheral);
+      if (p) {
+        p.connected = false;
+        return new Map(map.set(event.peripheral, p));
+      }
+      return map;
+    });
+  };
+
+  const handleConnectPeripheral = (event: any) => {
+    console.log(`[handleConnectPeripheral][${event.peripheral}] connected.`);
+  };
+
+  const handleUpdateValueForCharacteristic = (
+    //Adjust this functions
+    data: BleManagerDidUpdateValueForCharacteristicEvent,
+  ) => {
+    console.debug(
+      `[handleUpdateValueForCharacteristic] received data from '${data.peripheral}' with characteristic='${data.characteristic}' and value='${data.value}'`,
+    );
+    if (
+      data.service === SERVICE_UUIDS[0] &&
+      data.characteristic === CHARACTERISTIC_UUID
+    ) {
+      const esp32response: number[] = data.value;
+      const esp32classID = Buffer.from(esp32response).toString("utf-8");
+    }
+  };
+
+  const handleDiscoverPeripheral = (peripheral: Peripheral) => {
+    console.debug("[handleDiscoverPeripheral] new BLE peripheral=", peripheral);
+    if (!peripheral.name) {
+      peripheral.name = "NO NAME";
+    }
+    setPeripherals(map => {
+      return new Map(map.set(peripheral.id, peripheral));
+    });
+  };
+
+  const handleAndroidPermissions = () => {
+    if (Platform.OS === "android" && Platform.Version >= 31) {
+      PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      ]).then(result => {
+        if (result) {
+          console.debug(
+            "[handleAndroidPermissions] User accepts runtime permissions android 12+",
+          );
+        } else {
+          console.error(
+            "[handleAndroidPermissions] User refuses runtime permissions android 12+",
+          );
+        }
+      });
+    } else if (Platform.OS === "android" && Platform.Version >= 23) {
+      PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      ).then(checkResult => {
+        if (checkResult) {
+          console.debug(
+            "[handleAndroidPermissions] runtime permission Android <12 already OK",
+          );
+        } else {
+          PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          ).then(requestResult => {
+            if (requestResult) {
+              console.debug(
+                "[handleAndroidPermissions] User accepts runtime permission android <12",
+              );
+            } else {
+              console.error(
+                "[handleAndroidPermissions] User refuses runtime permission android <12",
+              );
+            }
+          });
+        }
+      });
+    }
+  };
+
+  const startScan = () => {
+    if (!isScanning) {
+      setPeripherals(new Map<Peripheral["id"], Peripheral>());
+
+      try {
+        console.debug("[startScan] starting scan...");
+        setIsScanning(true);
+        BleManager.scan(SERVICE_UUIDS, SECONDS_TO_SCAN_FOR, ALLOW_DUPLICATES, {
+          matchMode: BleScanMatchMode.Sticky,
+          scanMode: BleScanMode.LowLatency,
+          callbackType: BleScanCallbackType.AllMatches,
+          exactAdvertisingName: ["Nordic_LLPM"], //change name to device if needed
+        })
+          .then(() => {
+            console.debug("[startScan] scan promise returned successfully.");
+          })
+          .catch((err: any) => {
+            console.error("[startScan] ble scan returned in error", err);
+          });
+      } catch (error) {
+        console.error("[startScan] ble scan error thrown", error);
+      }
+    }
+  };
+
+  const connectPeripheral = async (peripheral: Peripheral) => {
+    try {
+      if (peripheral) {
+        setPeripherals(map => {
+          let p = map.get(peripheral.id);
+          if (p) {
+            p.connecting = true;
+            return new Map(map.set(p.id, p));
+          }
+          return map;
+        });
+
+        await BleManager.connect(peripheral.id);
+        console.debug(`[connectPeripheral][${peripheral.id}] connected.`);
+
+        setPeripherals(map => {
+          let p = map.get(peripheral.id);
+          if (p) {
+            p.connecting = false;
+            p.connected = true;
+            return new Map(map.set(p.id, p));
+          }
+          return map;
+        });
+
+        // before retrieving services, it is often a good idea to let bonding & connection finish properly
+        await sleep(900);
+
+        /* Test read current RSSI value, retrieve services first */
+        const peripheralData = await BleManager.retrieveServices(peripheral.id);
+        console.debug(
+          `[connectPeripheral][${peripheral.id}] retrieved peripheral services`,
+          peripheralData,
+        );
+
+        const rssi = await BleManager.readRSSI(peripheral.id);
+        console.debug(
+          `[connectPeripheral][${peripheral.id}] retrieved current RSSI value: ${rssi}.`,
+        );
+
+        if (peripheralData.characteristics) {
+          for (let characteristic of peripheralData.characteristics) {
+            if (characteristic.descriptors) {
+              for (let descriptor of characteristic.descriptors) {
+                try {
+                  let data = await BleManager.readDescriptor(
+                    peripheral.id,
+                    characteristic.service,
+                    characteristic.characteristic,
+                    descriptor.uuid,
+                  );
+                  console.debug(
+                    `[connectPeripheral][${peripheral.id}] ${characteristic.service} ${characteristic.characteristic} ${descriptor.uuid} descriptor read as:`,
+                    data,
+                  );
+                } catch (error) {
+                  console.error(
+                    `[connectPeripheral][${peripheral.id}] failed to retrieve descriptor ${descriptor} for characteristic ${characteristic}:`,
+                    error,
+                  );
+                }
+              }
+            }
+          }
+        }
+
+        setPeripherals(map => {
+          let p = map.get(peripheral.id);
+          if (p) {
+            p.rssi = rssi;
+            return new Map(map.set(p.id, p));
+          }
+          return map;
+        });
+      }
+    } catch (error) {
+      console.error(
+        `[connectPeripheral][${peripheral.id}] connectPeripheral error`,
+        error,
+      );
+    }
+  };
+
+  const renderItem = ({item}: {item: Peripheral}) => {
+    const backgroundColor = item.connected ? "#069400" : "#fffff";
+    return (
+      <TouchableHighlight
+        underlayColor="#0082FC"
+        onPress={() => connectPeripheral(item)}>
+        <View style={[styles.row, {backgroundColor}]}>
+          <Text style={styles.peripheralName}>
+            {/* completeLocalName (item.name) & shortAdvertisingName (advertising.localName) may not always be the same */}
+            {item.name} - {item?.advertising?.localName}
+            {item.connecting && " - Starting lecture..."}
+          </Text>
+          <Text style={styles.peripheralId}>{item.id}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Button
         style={styles.button}
         labelStyle={styles.buttonText}
+        onPress={startScan}
         mode="contained">
-        Check me!
+        Look for nearby classrooms!
       </Button>
+      <FlatList
+        data={Array.from(peripherals.values())}
+        contentContainerStyle={{rowGap: 12}}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
       <Button
         style={styles.logoutButton}
         labelStyle={styles.buttonText}
@@ -113,5 +380,31 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     margin: 10,
     backgroundColor: "red",
+  },
+
+  peripheralName: {
+    fontSize: 16,
+    textAlign: "center",
+    padding: 10,
+  },
+  rssi: {
+    fontSize: 12,
+    textAlign: "center",
+    padding: 2,
+  },
+  peripheralId: {
+    fontSize: 12,
+    textAlign: "center",
+    padding: 2,
+    paddingBottom: 20,
+  },
+  row: {
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 20,
+  },
+  noPeripherals: {
+    margin: 10,
+    textAlign: "center",
   },
 });
